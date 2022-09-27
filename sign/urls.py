934bb95f4f036e075d9615e85ca9165b3422756a
Name <email@example.ru>
2022-09-27 21:48:41 +0300

    modified:   NewsPaper/settings.py 	modified:   news/views.py 	modified:   sign/urls.py 	modified:   templates/main.html 	modified:   templates/news.html 	modified:   templates/post_create.html 	modified:   templates/post_delete.html 	modified:   templates/post_detail.html 	modified:   templates/post_edit.html 	modified:   templates/profile_edit.html 	modified:   templates/search.html 	modified:   templates/sign/account.html 	modified:   templates/sign/login.html 	modified:   templates/sign/logout.html 	modified:   templates/sign/signup.html 	modified:   templates/sign/update_profile.html 	modified:   templates/subscribe_new_post.html

--- sign/urls.py
+++ sign/urls.py
@@ -8,9 +8,4 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name = 'sign/logout.html'), name='logout'),
     path('signup/', BaseRegisterView.as_view(template_name = 'sign/signup.html'), name='signup'),
     path('add_authors/', add_authors, name = 'upgrade'),
-
 ]
-
-
-
-
